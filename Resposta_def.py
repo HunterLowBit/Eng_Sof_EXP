66da8e56b7d4a57d26b792c777abccf281ec7303
HunterLowBit_Win11VsCode <HunterLowBit@users.noreply.github.com>
2024-05-12 11:52:05 -0300

    REWORK

--- Resposta_def.py
+++ Resposta_def.py
@@ -1,6 +1,7 @@
 from scipy.stats import chi2_contingency
 import numpy as np
 
+
 data = np.array([[156, 124, 77, 82, 53, 32, 33, 12, 10, 13, 8, 0, 0],
     [14, 20, 11, 36, 11, 24, 23, 46, 51, 13, 1, 3, 1],
     [2, 5, 7, 15, 1, 4, 9, 23, 75, 21, 53, 160, 6],
@@ -9,9 +10,9 @@ data = np.array([[156, 124, 77, 82, 53, 32, 33, 12, 10, 13, 8, 0, 0],
 chi2, p, dof, expected = chi2_contingency(data)
 
 with open("Resposta.txt", "w") as f:
-    f.write(f"Chi-squared statistic: {chi2:.8f}\n")
-    f.write(f"P-value: {p:.8f}\n")
-    f.write(f"Degrees of freedom: {dof:.8f}\n")
+    f.write(f"Chi-squared statistic: {chi2:.13f}\n")
+    f.write(f"P-value: {p:.2f}\n")
+    f.write(f"Degrees of freedom: {dof:.2f}\n")
     f.write(f"Expected frequencies: \n")
     np.savetxt(f, expected, fmt='%1.8f')
 
